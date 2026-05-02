b8f51a606984eda6b4b4ec374ec24d6a0b782dfb
carlosvts <carlosvtsdev@gmail.com>
2026-05-02 14:15:59 -0300

    feat: prototipo 2, primeira janela da UI

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,12 +1,16 @@
 use std::time::Duration;
 
 use color_eyre::Result;
+use crossterm::cursor::position;
 use ratatui::crossterm::event::{self, Event};
+use ratatui::layout::{Alignment, Position};
+use ratatui::widgets::Paragraph;
 use ratatui::{
     DefaultTerminal, Frame,
-    layout::{Constraint, Layout, Spacing},
+    layout::{Constraint, Direction, Layout, Spacing},
     style::{Color, Style},
     symbols::merge::MergeStrategy,
+    text::{Line, Span},
     widgets::Block,
 };
 
@@ -33,35 +37,97 @@ fn key_pressed() -> Result<bool> {
 }
 
 fn draw(frame: &mut Frame) {
-    // espaçamentos
-    // dois retangulos da direita
-    let [left, right] = Layout::horizontal([Constraint::Fill(1); 2])
+    let outer_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Percentage(5),
+            Constraint::Fill(1),
+            Constraint::Percentage(5),
+        ])
         .spacing(Spacing::Overlap(1))
-        .areas(frame.area());
+        .split(frame.area());
 
-    // retangulo da esquerda
-    let [top_right, bottom_right] = Layout::vertical([Constraint::Fill(1); 2])
-        .spacing(Spacing::Overlap(1))
-        .areas(right);
+    let inner_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(outer_layout[1]);
 
     // blocos
-    let left_block = Block::bordered()
-        .title("Traceback")
+    let up_block = Block::bordered()
+        .title("CONVERSOR UNIVERSAL DE BASES")
         .border_style(Style::default().fg(Color::LightRed))
         .merge_borders(MergeStrategy::Exact);
 
-    let top_right_block = Block::bordered()
-        .title("Resultado")
+    let middle_block_left = Block::bordered()
+        .title("ENTRADA")
+        .border_style(Style::default().fg(Color::LightYellow))
+        .merge_borders(MergeStrategy::Exact);
+
+    let middle_block_right = Block::bordered()
+        .title("SAIDA")
         .border_style(Style::default().fg(Color::LightGreen))
         .merge_borders(MergeStrategy::Exact);
 
     let bottom_right_block = Block::bordered()
-        .title("Input")
+        .title("Info")
         .border_style(Style::default().fg(Color::LightBlue))
         .merge_borders(MergeStrategy::Exact);
 
+    let up_inner = up_block.inner(outer_layout[0]);
+    let left_inner = middle_block_left.inner(inner_layout[0]);
+    let right_inner = middle_block_right.inner(inner_layout[1]);
+    let bottom_inner = bottom_right_block.inner(outer_layout[2]);
+
     // display
-    frame.render_widget(left_block, left);
-    frame.render_widget(top_right_block, top_right);
-    frame.render_widget(bottom_right_block, bottom_right);
+    frame.render_widget(up_block, outer_layout[0]);
+    frame.render_widget(middle_block_left, inner_layout[0]);
+    frame.render_widget(middle_block_right, inner_layout[1]);
+    frame.render_widget(bottom_right_block, outer_layout[2]);
+
+    let title = Paragraph::new(Line::from(vec![Span::raw(
+        "[1] conversor    [2] trace    [3] quiz    [4] batch    [5] máximos",
+    )]))
+    .alignment(Alignment::Center)
+    .style(Style::default().fg(Color::DarkGray));
+    frame.render_widget(title, up_inner);
+
+    // input
+    let input = Paragraph::new(vec![
+        Line::from(vec![
+            Span::styled("Valor : ", Style::default().fg(Color::DarkGray)),
+            Span::styled("42", Style::default().fg(Color::White)),
+            Span::styled("█", Style::default().fg(Color::LightYellow)), // cursor
+        ]),
+        Line::from(""),
+        Line::from(vec![
+            Span::styled("De    : ", Style::default().fg(Color::DarkGray)),
+            Span::styled("[DEC]", Style::default().fg(Color::LightYellow)),
+        ]),
+        Line::from(vec![
+            Span::styled("Para  : ", Style::default().fg(Color::DarkGray)),
+            Span::styled("[BIN]", Style::default().fg(Color::LightYellow)),
+        ]),
+        Line::from(""),
+        Line::from(Span::styled(
+            "[enter] converter",
+            Style::default().fg(Color::LightGreen),
+        )),
+        Line::from(Span::styled(
+            "[s] passo a passo",
+            Style::default().fg(Color::DarkGray),
+        )),
+    ]);
+    frame.render_widget(input, left_inner);
+    // output
+    // TODO
+
+    // rodapé
+    let info = Paragraph::new(Line::from(vec![
+        Span::styled("status: ", Style::default().fg(Color::DarkGray)),
+        Span::styled("OK", Style::default().fg(Color::LightGreen)),
+        Span::raw("  │  modo: conversor  │  histórico: 3"),
+    ]))
+    .alignment(Alignment::Center)
+    .style(Style::default().fg(Color::DarkGray));
+    frame.render_widget(info, bottom_inner);
 }
