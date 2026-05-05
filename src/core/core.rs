84d84e262b8b0dbd59a5a550447a3d881041d709
carlosvts <carlosvtsdev@gmail.com>
2026-05-05 15:19:44 -0300

    feat: sepracao entre frontend e backend

---
+++ src/core/core.rs
@@ -0,0 +1,8 @@
+// Definir as structs aqui e fazer a lógica de backend aqui
+
+// Para rodar somente esse código como forma de debug, só usar
+// cargo run -bin --core-debug
+
+fn main() {
+    println!("Hello World!");
+}
