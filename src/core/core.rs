f7cd0a09778b98bac05bed1cc55bed7f80174622
carlosvts <carlosvtsdev@gmail.com>
2026-05-05 15:36:54 -0300

    feat: funcao inicial de parse de csv

--- src/core/core.rs
+++ src/core/core.rs
@@ -3,6 +3,18 @@
 // Para rodar somente esse código como forma de debug, só usar
 // cargo run -bin --core-debug
 
+use csv::Reader;
+use std::fs::File; //std filestream file 
+
+// Recebe um path de arquivo, retorna um reader de CSV (um iterador)
+#[warn(dead_code)] // coloquei a flag pro compilador nao reclamar
+fn from_csv(path: String) -> Result<Reader<File>, csv::Error> {
+    let reader: Reader<File> = Reader::from_path(path)?;
+    Ok(reader)
+}
+
+// daqui poderiamos, por exemplo, criar outras funcoes etc,
+
 fn main() {
     println!("Hello World!");
 }
