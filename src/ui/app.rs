fec78a31e244c192589c77f6e21821476f4a528d
carlosvts <carlosvtsdev@gmail.com>
2026-05-09 16:09:56 -0300

    feat: add mock de testes e interface para modo Batch

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,16 +1,16 @@
 use color_eyre::Result;
 use ratatui::crossterm::event::{self, Event, KeyCode};
-use ratatui::layout::{Alignment, Position};
+use ratatui::layout::Alignment;
 use ratatui::style::Modifier;
-use ratatui::widgets::Paragraph;
 use ratatui::{
     DefaultTerminal, Frame,
     layout::{Constraint, Direction, Layout, Rect, Spacing},
     style::{Color, Style},
     symbols::merge::MergeStrategy,
     text::{Line, Span},
-    widgets::Block,
+    widgets::{Block, Borders, List, ListItem, Paragraph},
 };
+use std::fs;
 
 #[repr(i32)] // representavel como i32
 #[derive(PartialEq, Eq, Clone, Copy)] // necessario para comparação com inteiros
@@ -34,14 +34,20 @@ struct TraceState {
     bits: Vec<u8>,
 }
 
+struct BatchState {
+    selected_file: usize,
+}
+
 struct App {
     tab: Tabs,
+    pub batch: BatchState,
 }
 
 impl App {
     fn new() -> Self {
         Self {
             tab: Tabs::Conversor,
+            batch: BatchState { selected_file: 0 },
         }
     }
 }
@@ -55,12 +61,28 @@ fn main() -> Result<()> {
 
         if let Event::Key(key) = event::read()? {
             match key.code {
+                // troca abas
                 KeyCode::Char('1') => app.tab = Tabs::Conversor,
                 KeyCode::Char('2') => app.tab = Tabs::Trace,
                 KeyCode::Char('3') => app.tab = Tabs::Quiz,
                 KeyCode::Char('4') => app.tab = Tabs::Batch,
                 KeyCode::Char('5') => app.tab = Tabs::Max,
                 KeyCode::Char('q') => break,
+
+                // navegação batch
+                KeyCode::Down => {
+                    if app.tab == Tabs::Batch {
+                        app.batch.selected_file += 1;
+                    }
+                }
+
+                KeyCode::Up => {
+                    if app.tab == Tabs::Batch {
+                        if app.batch.selected_file > 0 {
+                            app.batch.selected_file -= 1;
+                        }
+                    }
+                }
                 _ => {}
             }
         }
@@ -132,13 +154,7 @@ fn draw(frame: &mut Frame, app: &App) {
         //    left_inner,
         //    right_inner,
         //),
-        //Tabs::Batch => draw_batch(
-        //    frame,
-        //    middle_block_left,
-        //    middle_block_right,
-        //    left_inner,
-        //    right_inner,
-        //),
+        Tabs::Batch => draw_batch(frame, left_inner, right_inner, app),
         //Tabs::Max => draw_max(
         //    frame,
         //    middle_block_right,
@@ -321,4 +337,98 @@ fn draw_trace(frame: &mut Frame, left_inner: Rect, right_inner: Rect, tracestate
 
     frame.render_widget(left, left_inner);
     frame.render_widget(right, right_inner);
+}
+
+fn draw_batch(frame: &mut Frame, left_inner: Rect, right_inner: Rect, app: &App) {
+    // LEITURA DOS CSV
+    let mut entries: Vec<String> = Vec::new();
+
+    if let Ok(read_dir) = fs::read_dir("./data") {
+        for entry in read_dir {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+
+                // aceita apenas arquivos .csv
+                if path.is_file() {
+                    if let Some(extension) = path.extension() {
+                        let ext = extension.to_string_lossy().to_lowercase();
+                        if ext == "csv" {
+                            if let Some(name) = path.file_name() {
+                                entries.push(name.to_string_lossy().to_string());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // seleção atual
+    let mut selected_index: usize = app.batch.selected_file;
+
+    // impede overflow
+    if selected_index >= entries.len() && entries.len() > 0 {
+        selected_index = entries.len() - 1;
+    }
+
+    // =========================
+    // LISTA DE ARQUIVOS
+    // =========================
+
+    let mut items: Vec<ListItem> = Vec::new();
+
+    for i in 0..entries.len() {
+        let style;
+
+        if i == selected_index {
+            style = Style::default()
+                .fg(Color::Black)
+                .bg(Color::LightRed)
+                .add_modifier(Modifier::BOLD);
+        } else {
+            style = Style::default().fg(Color::Gray);
+        }
+
+        let line = Line::from(vec![Span::styled(format!(" {}", entries[i]), style)]);
+
+        items.push(ListItem::new(line));
+    }
+
+    let file_list = List::new(items).block(
+        Block::default()
+            .title(" Arquivos CSV ")
+            .borders(Borders::NONE),
+    );
+
+    frame.render_widget(file_list, left_inner);
+
+    // PREVIEW
+    let mut preview_lines: Vec<Line> = Vec::new();
+
+    preview_lines.push(Line::from("Arquivo selecionado:"));
+    preview_lines.push(Line::from(""));
+    preview_lines.push(Line::from(format!(
+        "Quantidade de arquivos {}",
+        entries.len()
+    )));
+    if entries.len() > 0 {
+        preview_lines.push(Line::from(vec![Span::styled(
+            &entries[selected_index],
+            Style::default()
+                .fg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )]));
+
+        preview_lines.push(Line::from(""));
+        preview_lines.push(Line::from("[enter] converter lote"));
+    } else {
+        preview_lines.push(Line::from(vec![Span::styled(
+            "    Nenhum arquivo CSV encontrado",
+            Style::default().fg(Color::DarkGray),
+        )]));
+    }
+
+    let preview = Paragraph::new(preview_lines);
+
+    frame.render_widget(preview, right_inner);
 }
